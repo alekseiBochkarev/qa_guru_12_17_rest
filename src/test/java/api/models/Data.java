aae7cbcd717460346bfd8728925a6b22eabd8cd3
Aleksei Bochkarev <aleksei.bochkarev@brandmaker.com>
2022-07-07 20:09:12 +0700

    removed nested classes

---
+++ src/test/java/api/models/Data.java
@@ -0,0 +1,9 @@
+package api.models;
+
+public class Data {
+    public int id;
+    public String email;
+    public String first_name;
+    public String last_name;
+    public String avatar;
+}
---
+++ src/test/java/api/models/Support.java
@@ -0,0 +1,6 @@
+package api.models;
+
+public class Support {
+    public String url;
+    public String text;
+}
